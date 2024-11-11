31482bffbcd268a60754f4366d3d2a8fe7729b97
Jiwoo Hong <jiwooh@postech.ac.kr>
2024-11-11 21:17:58 +0900

    implement PA7 Part 2

--- nodescala/src/main/scala/nodescala/nodescala.scala
+++ nodescala/src/main/scala/nodescala/nodescala.scala
@@ -29,7 +29,10 @@ trait NodeScala {
    *  @param token        the cancellation token
    *  @param body         the response to write back
    */
-  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = ???
+  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = {
+    while (!token.isCancelled && response.hasNext) exchange.write(response.next)
+    exchange.close()
+  }
 
   /** A server:
    *  1) creates and starts an http listener
@@ -41,7 +44,20 @@ trait NodeScala {
    *  @param handler        a function mapping a request to a response
    *  @return               a subscription that can stop the server and all its asynchronous operations *entirely*
    */
-  def start(relativePath: String)(handler: Request => Response): Subscription = ???
+  def start(relativePath: String)(handler: Request => Response): Subscription = {
+    val listener = createListener(relativePath)
+    val listenerSubscription = listener.start()
+    val responderSubscription = Future.run() { cancelToken => Future {
+        async {
+          while (!cancelToken.isCancelled) {
+            val (req, exch) = await(listener.nextRequest())
+            respond(exch, cancelToken, handler(req))
+          }
+        }
+      }
+    }
+    Subscription(listenerSubscription, responderSubscription)
+  }
 
 }
 
